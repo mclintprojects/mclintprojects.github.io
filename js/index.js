3e4cbb2227fa19038fc1d7a388f1f7b52853c7c0
Mbah Clinton <clintonmbah44@gmail.com>
2018-03-27 00:36:15 +0100

    Redesign baby!

--- js/index.js
+++ js/index.js
@@ -12,3 +12,10 @@ $(document).ready(function() {
 		$('#contact-msg').attr('value', '');
 	});
 });
+
+$(function() {
+	$(document).scroll(function() {
+		var $nav = $('.navbar-fixed-top');
+		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
